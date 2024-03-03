53d1e38d98f0c01dc9c71d43435b7a0d47add2b9
kishorekumar1111 <kishore.kumar@kalvium.community>
2024-03-03 19:29:34 +0530

    done

---
+++ props/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import GallaryFooter from './components/GallaryFooter';
+import Main from './components/Main';
+import Navbar from './components/Navbar' 
+
+<imageData />
+
+function App() {
+  return (
+    <div>
+     <Navbar />
+
+     <Main />
+      {/* adding footer component */}
+      <GallaryFooter/> 
+    </div>
+  )
+}
+
+export default App;
